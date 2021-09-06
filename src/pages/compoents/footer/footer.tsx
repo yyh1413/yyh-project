f53bed6c6b514dd5be22ab60f0ffc779258e3bf7
yangyh <yuhao.yang1@pactera.com>
2021-09-06 20:35:55 +0800

    页面兼容性优化

--- src/pages/compoents/footer/footer.tsx
+++ src/pages/compoents/footer/footer.tsx
@@ -1,11 +1,21 @@
 import React from "react"
-
+import beian from '../../../static/beian.png'
 import './footer.less'
 const Footer: React.FC = () => {
 
     return (
         <div id="footer">
-            <div>©Copyright 2021 | 由杨宇豪服务提供™。</div>
+            <div className='footer_flex'>
+                <div className='footer_flex_beian'>
+                    <span className='beian'>备案号：</span>
+                    <a className='beian' href="https://beian.miit.gov.cn/" target="_blank">辽ICP备2021009135号-1</a>
+                </div>
+                <div  className='footer_flex_beian'>
+                    <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=2021009135">
+                        <img src={beian} />
+                        辽公网安备 2021009135号</a>
+                </div>
+            </div>
             {/*<div> </div>*/}
         </div>
     )
